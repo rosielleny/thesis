bfb0bb268812f0cfdbe0aac5aa0ff366fa8eb801
rosielleny <r-e-young@outlook.com>
2024-07-23 11:30:53 +0100

    Plant Microservice Resource

--- PlantMicroService/src/main/java/org/plant/service/PlantService.java
+++ PlantMicroService/src/main/java/org/plant/service/PlantService.java
@@ -1,6 +1,8 @@
 package org.plant.service;
 
+import org.plant.entity.CompendiumPage;
 import org.plant.entity.Plant;
+
 import java.util.List;
 
 public interface PlantService{
@@ -8,4 +10,5 @@ public interface PlantService{
     List<Plant> getAllPlants();
     Plant getPlantById(int id);
     Plant getPlantByName(String name);
+    CompendiumPage getCompendiumPageByPlantId(int plantId);
 }
