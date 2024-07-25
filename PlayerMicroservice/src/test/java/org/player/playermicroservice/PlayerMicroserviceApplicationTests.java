1bbfd033f722d418e780877c8497cdb1dc695af0
rosielleny <r-e-young@outlook.com>
2024-07-25 15:51:59 +0100

    Player Microservice

---
+++ PlayerMicroservice/src/test/java/org/player/playermicroservice/PlayerMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.player.playermicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
